3e4f04139933c0a908973b003c169748f9cd2443
Jared Brown <jaredbrown1@hotmail.com>
2023-11-08 19:44:25 -0800

    planet image responsive

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -12,29 +12,35 @@ export function generateStaticParams() {
 const PlanetInfo = ({ params }) => {
 	const planet = data.find((p) => p.name.toString() === params.id);
 
-	console.log("planet", planet);
-
 	if (!planet) {
 		return <div>Not found</div>;
 	}
 
 	return (
-		<div className="text-white">
-			<div className="w-[500px] h-[500px] p-4">
-				<Image
-					src={planet.images.planet}
-					width={145}
-					height={290}
-					alt="planet image"
-				/>
-				<h1 className="text-[80px]">{planet.name}</h1>
-				<p>{planet.overview["content"]}</p>
-				<p>
-					Source:{" "}
-					<span>
-						<Link href={planet.overview["source"]}>Wikipedia</Link>
-					</span>
-				</p>
+		<div className="flex flex-col justify-center items-center text-white pt-[96px]">
+			<div className="flex justify-between text-left items-center p-4">
+				<div className="relative">
+					<Image
+						src={planet.images.planet}
+						width={10}
+						height={10}
+						alt="planet image"
+						layout="responsive"
+					/>
+				</div>
+
+				<div className="pl-[153px]">
+					<h1 className="text-[80px]">{planet.name}</h1>
+					<p className="text-[14px] w-[350px] leading-[25px]">
+						{planet.overview["content"]}
+					</p>
+					<p className="pt-[49px]">
+						Source:
+						<span>
+							<Link href={planet.overview["source"]}>Wikipedia</Link>
+						</span>
+					</p>
+				</div>
 			</div>
 			<div className="flex">
 				<div className="w-[255px] h-[128px]">
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 
+import { Antonio } from "next/font/google";
+
+const ant = Antonio({ subsets: ["latin"] });
+
 const Navbar = () => {
 	return (
 		<div className="flex justify-between w-full p-6 border-b-[1px] border-gray-700 text-white">
-			<h1 className="text-[28px]">THE PLANETS</h1>
+			<h1 className={`${ant.className} text-[28px]`}>THE PLANETS</h1>
 			<div className="flex justify-around w-[650px] items-center">
 				<Link href="/Mercury">MERCURY</Link>
 				<Link href="/Venus">VENUS</Link>
